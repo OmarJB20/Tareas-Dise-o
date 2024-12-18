aae8cad530e443d2c302b72f585e52a681d887f2
OmarJB20 <ojc.santillan@yavirac.edu.ec>
2024-12-18 17:44:14 -0500

    Mejoras

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -73,7 +73,27 @@ export class GaleriaComponent implements OnInit {
         thumbnailImageSrc: 'https://static4.depositphotos.com/1005366/368/i/600/depositphotos_3688121-stock-photo-sunset-over-water.jpg',
         alt: 'Imagen 5',
         title: 'Imagen 5',
-      }
+      },
+      {
+        itemImageSrc: 'https://st3.depositphotos.com/15076092/17433/i/600/depositphotos_174336744-stock-photo-beautiful-red-sunset-and-sea.jpg',
+        thumbnailImageSrc: 'https://st3.depositphotos.com/15076092/17433/i/600/depositphotos_174336744-stock-photo-beautiful-red-sunset-and-sea.jpg',
+        alt: 'Imagen 3',
+        title: 'Imagen 3',
+      },
+      {
+        itemImageSrc: 'https://st.depositphotos.com/2045405/2015/i/600/depositphotos_20157387-stock-photo-summer-landscape-beauty-sunset-over.jpg',
+        thumbnailImageSrc: 'https://st.depositphotos.com/2045405/2015/i/600/depositphotos_20157387-stock-photo-summer-landscape-beauty-sunset-over.jpg',
+        alt: 'Imagen 2',
+        title: 'Imagen 2',
+      },
+      {
+        itemImageSrc: 'https://st.depositphotos.com/3095291/4120/i/600/depositphotos_41209319-stock-photo-beach-at-sunset.jpg',
+        thumbnailImageSrc: 'https://st.depositphotos.com/3095291/4120/i/600/depositphotos_41209319-stock-photo-beach-at-sunset.jpg',
+        alt: 'Imagen 4',
+        title: 'Imagen 4',
+      },
+      
+
     ];
 
     this.responsiveOptions = [
